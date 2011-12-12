ff72e598f13277d89024354bb802bffc0f456b10
mwoodbri <mwoodbri@a87e785b-2db9-4049-9ecb-37aebffb8c9b>
2011-12-12 16:49:56 +0000

    Fix bad imports

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,12 +1,5 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
-
-import java.util.*;
-
-import models.*;
-
 public class Application extends SecureController {
 
     public static void index() {
