2e91b66491aa271a26598bd0bb4ad0b3c80ca6c6
mwoodbri <mwoodbri@a87e785b-2db9-4049-9ecb-37aebffb8c9b>
2011-12-19 15:40:00 +0000

    Add image browsing

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -4,16 +4,31 @@ import java.io.IOException;
 import java.util.List;
 
 import org.dcm4che2.data.DicomObject;
+import org.dcm4che2.data.Tag;
 import org.dcm4che2.net.ConfigurationException;
 
 import util.DicomQuery;
 
 public class Application extends SecureController {
 
-    public static void index(String name) throws IOException, ConfigurationException, InterruptedException {
+    public static void index(String name) throws Exception {
     	params.flash();
-    	List<DicomObject> patients = DicomQuery.query(name);
+    	List<DicomObject> patients = DicomQuery.patient(name);
         render(patients);
     }
 
+    public static void patient(String id) throws Exception {
+    	List<DicomObject> studies = DicomQuery.studies(id);
+    	render(studies);
+    }
+    
+    public static void study(String id) throws Exception {
+    	List<DicomObject> seriess = DicomQuery.seriess(id);
+    	render(seriess);
+    }
+    
+    public static void series(String id) throws Exception {
+    	List<DicomObject> images = DicomQuery.images(id);
+    	render(images);
+    }
 }
