d20777b89f5d0326c690b3c8ec1e1ecc98baf1f4
mwoodbri <mwoodbri@a87e785b-2db9-4049-9ecb-37aebffb8c9b>
2012-02-01 15:55:08 +0000

    Revert to using database for archive location

--- app/util/Properties.java
+++ app/util/Properties.java
@@ -2,11 +2,8 @@ package util;
 
 import java.io.File;
 
-import org.w3c.dom.Document;
-
+import models.Filesystem;
 import play.Play;
-import play.libs.XML;
-import play.libs.XPath;
 
 public class Properties {
 
@@ -19,8 +16,7 @@ public class Properties {
 	}
 
 	public static File getArchive() {
-		Document document = XML.getDocument(new File(getString("dcm4chee"), "server/default/data/xmbean-attrs/dcm4chee.archive@3Aservice@3DFileSystemMgt@2Cgroup@3DONLINE_STORAGE.xml"));
-		File archive = new File(XPath.selectText("/attribute-list/attribute[@name='DefaultStorageDirectory']", document));
-		return archive.isAbsolute() ? archive : new File(getString("dcm4chee"), String.format("server/default/%s", archive));
+		File archive = new File(Filesystem.<Filesystem>findAll().get(0).dirpath);
+		return archive.isAbsolute() ? archive : new File(getString("dcm4chee"), String.format("server/default/%s", archive.getPath()));
 	}
 }
