a4823039a45247b6807ba7419627a6524f18b191
mwoodbri <mwoodbri@a87e785b-2db9-4049-9ecb-37aebffb8c9b>
2012-02-10 15:48:37 +0000

    Fix versioning in release script

--- bin/release.sh
+++ bin/release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ex
 rm -fr /tmp/MRIdb
-svn export https://cisbic.bioinformatics.ic.ac.uk/svn/MRIdb/mridb /tmp/MRIdb
+REV=$(svn export https://cisbic.bioinformatics.ic.ac.uk/svn/MRIdb/mridb /tmp/MRIdb | grep "^Exported revision" | egrep -o "[0-9]+")
 play dependencies /tmp/MRIdb --forceCopy --forProd
 play precompile /tmp/MRIdb
-tar cfz /tmp/MRIdb-r$(svn info ~/workspace/MRIdb | grep "^Revision: " | cut -d' ' -f2).tar.gz -C /tmp MRIdb
+tar cfz /tmp/MRIdb-r$REV.tar.gz -C /tmp MRIdb
