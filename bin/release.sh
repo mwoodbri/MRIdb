3b1759961dde293a8c22d444235fbcac1489d673
mwoodbri <mwoodbri@a87e785b-2db9-4049-9ecb-37aebffb8c9b>
2012-02-27 15:03:43 +0000

    Sync release script with ADAM

--- bin/release.sh
+++ bin/release.sh
@@ -1,10 +1,14 @@
 #!/bin/bash -ex
-rm -fr /tmp/MRIdb
-REV=$(svn export https://cisbic.bioinformatics.ic.ac.uk/svn/MRIdb/mridb /tmp/MRIdb | grep "^Exported revision" | egrep -o "[0-9]+")
-play dependencies /tmp/MRIdb --forceCopy --forProd
-play precompile /tmp/MRIdb
-tar cfz /tmp/MRIdb-r$REV.tar.gz -C /tmp MRIdb
 
-DEST=/data/www/80/cisbic.bioinformatics.ic.ac.uk/htdocs/static/mridb
-scp /tmp/MRIdb-r$REV.tar.gz bss-srv4:$DEST
+prog="mridb"
+
+rm -fr /tmp/$prog
+REV=$(svn export https://cisbic.bioinformatics.ic.ac.uk/svn/MRIdb/$prog /tmp/$prog | grep "^Exported revision" | egrep -o "[0-9]+")
+play dependencies /tmp/$prog --forceCopy --forProd
+play precompile /tmp/$prog
+#find /tmp/$prog/app -mindepth 1 -maxdepth 1 ! -iname views -exec rm -r {} \;
+tar cfj /tmp/$prog-r$REV.tar.bz2 -C /tmp $prog
+
+DEST=/data/www/80/cisbic.bioinformatics.ic.ac.uk/htdocs/static/$prog
+scp /tmp/$prog-r$REV.tar.bz2 bss-srv4:$DEST
 ssh bss-srv4 "sed -i 's/r[0-9]\+/r$REV/' $DEST/.htaccess"
