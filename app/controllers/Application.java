e5d1ae62932b144ccb5f952bceb109061b7f1ce7
mwoodbri <mwoodbri@a87e785b-2db9-4049-9ecb-37aebffb8c9b>
2012-06-08 12:13:43 +0000

    Actually use 'missing' image

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -187,7 +187,7 @@ public class Application extends SecureController {
 		HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
 		if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
 			Logger.warn("Image not found for series %s", series);
-			renderBinary(new File(Play.applicationPath, "public/images/128x128.gif"));
+			renderBinary(new File(Play.applicationPath, "public/images/missing.png"));
 		}
 		IO.copy(new URL(url).openConnection().getInputStream(), response.out);
 	}
