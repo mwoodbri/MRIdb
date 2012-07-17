3c6f69ce5ee6cead0a28bb09db9cfe918b7ce876
mwoodbri <mwoodbri@a87e785b-2db9-4049-9ecb-37aebffb8c9b>
2012-07-17 17:50:54 +0000

    Deal with directories that don't contain any DICOMs

--- bin/import.sh
+++ bin/import.sh
@@ -19,12 +19,13 @@ echo "(Re)starting import at $(date) ($(wc -l < $1/import.txt) remaining)" >>$1/
 while :
 do
 	DIR=$(head -n1 $1/import.txt)
-	if [ -z "$DIR" ]
-	then
-		break
-	fi
 	echo -n "    Importing $DIR " >>$1/import.log
-	/opt/dcm4che-2.0.26/bin/dcmsnd DCM4CHEE@localhost:11112 $DIR >>$1/import.stdout 2>>$1/import.stderr && echo done >>$1/import.log && sed -i -e'1d' $1/import.txt || echo failed >>$1/import.log
+	if find $DIR -type f -exec file -ib {} + 2>/dev/null | grep dicom >/dev/null
+	then
+		/opt/dcm4che-2.0.26/bin/dcmsnd DCM4CHEE@localhost:11112 $DIR >>$1/import.stdout 2>>$1/import.stderr && echo done >>$1/import.log && sed -i -e'1d' $1/import.txt || echo failed >>$1/import.log
+	else
+		echo ignored >>$1/import.log && sed -i -e'1d' $1/import.txt
+	fi
 done
 
 echo "Import complete" >>$1/import.log
