04415a922a2de77411c017c3594f85559a5fe1e6
mwoodbri <mwoodbri@a87e785b-2db9-4049-9ecb-37aebffb8c9b>
2012-07-30 10:00:29 +0000

    Pause if import fails

--- bin/import.sh
+++ bin/import.sh
@@ -22,7 +22,7 @@ do
 	echo -n "    Importing $DIR " >>$1/import.log
 	if find $DIR -type f -exec file -ib {} + 2>/dev/null | grep dicom >/dev/null
 	then
-		/opt/dcm4che-2.0.26/bin/dcmsnd DCM4CHEE@localhost:11112 $DIR >>$1/import.stdout 2>>$1/import.stderr && echo done >>$1/import.log && sed -i -e'1d' $1/import.txt || echo failed >>$1/import.log
+		/opt/dcm4che-2.0.26/bin/dcmsnd DCM4CHEE@localhost:11112 $DIR >>$1/import.stdout 2>>$1/import.stderr && echo done >>$1/import.log && sed -i -e'1d' $1/import.txt || (echo failed >>$1/import.log && sleep 60)
 	else
 		echo ignored >>$1/import.log && sed -i -e'1d' $1/import.txt
 	fi
