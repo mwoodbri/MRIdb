52fd1aa10d06dec163ec3b01dd7db0ddf2896170
mwoodbri <mwoodbri@a87e785b-2db9-4049-9ecb-37aebffb8c9b>
2012-08-01 08:42:14 +0000

    Don't retry failed imports

--- bin/import.sh
+++ bin/import.sh
@@ -22,10 +22,11 @@ do
 	echo -n "    Importing $DIR " >>$1/import.log
 	if find $DIR -type f -exec file -ib {} + 2>/dev/null | grep dicom >/dev/null
 	then
-		/opt/dcm4che-2.0.26/bin/dcmsnd DCM4CHEE@localhost:11112 $DIR >>$1/import.stdout 2>>$1/import.stderr && echo done >>$1/import.log && sed -i -e'1d' $1/import.txt || (echo failed >>$1/import.log && sleep 60)
+		/opt/dcm4che-2.0.26/bin/dcmsnd DCM4CHEE@localhost:11112 $DIR >>$1/import.stdout 2>>$1/import.stderr && echo done >>$1/import.log || echo failed >>$1/import.log
 	else
-		echo ignored >>$1/import.log && sed -i -e'1d' $1/import.txt
+		echo ignored >>$1/import.log
 	fi
+	sed -i -e'1d' $1/import.txt
 done
 
 echo "Import complete" >>$1/import.log
