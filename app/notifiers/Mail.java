03a4c70e13b71d023c2fe928571e4775983acb59
mwoodbri <mwoodbri@a87e785b-2db9-4049-9ecb-37aebffb8c9b>
2013-06-25 22:05:06 +0000

    Add application name to subject of Exception emails

--- app/notifiers/Mail.java
+++ app/notifiers/Mail.java
@@ -1,5 +1,6 @@
 package notifiers;
 
+import play.Play;
 import play.mvc.Http.Request;
 import play.mvc.Mailer;
 import play.mvc.Scope.Session;
@@ -8,7 +9,7 @@ import util.Properties;
 public class Mail extends Mailer {
 
 	public static void exception(Request request, Session session, Throwable exception) {
-		setSubject("Exception");
+		setSubject(String.format("[%s] Exception", Play.configuration.getProperty("application.name")));
 		addRecipient(Properties.getString("mail.to"));
 		setFrom(String.format("%s <%s>", Properties.getString("application.name"), Properties.getString("mail.from")));
 		send(request, session, exception);
