c58ed50079e6566388edea1b8b09c4af648ca111
mwoodbri <mwoodbri@a87e785b-2db9-4049-9ecb-37aebffb8c9b>
2013-07-09 14:04:15 +0000

    No need to release zip of source

--- bin/release.sh
+++ bin/release.sh
@@ -7,8 +7,8 @@ rm -fr /tmp/$prog
 REV=$(svn export https://cisbic.bioinformatics.ic.ac.uk/svn/MRIdb/$prog /tmp/$prog | grep "^Exported revision" | egrep -o "[0-9]+")
 DEST=/data/www/80/cisbic.bioinformatics.ic.ac.uk/htdocs/files/$prog
 
-tar cfj /tmp/$prog-r$REV-src.tar.bz2 -C /tmp $prog
-scp /tmp/$prog-r$REV-src.tar.bz2 bss-srv4:$DEST
+#tar cfj /tmp/$prog-r$REV-src.tar.bz2 -C /tmp $prog
+#scp /tmp/$prog-r$REV-src.tar.bz2 bss-srv4:$DEST
 
 echo $(date +%Y%m%d)r$REV >/tmp/$prog/VERSION
 touch /tmp/$prog/conf/application-site.conf
