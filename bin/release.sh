ffe6f52c19b226c35aa2480f2b613e177605ef4a
Mark Woodbridge <m.woodbridge@imperial.ac.uk>
2013-08-05 17:01:10 +0100

    Git migration

--- bin/release.sh
+++ bin/release.sh
@@ -3,9 +3,9 @@
 prog="mridb"
 
 rm -fr /tmp/$prog
-
-REV=$(svn export https://cisbic.bioinformatics.ic.ac.uk/svn/MRIdb/$prog /tmp/$prog | grep "^Exported revision" | egrep -o "[0-9]+")
-
+mkdir /tmp/$prog
+git archive master | tar -x -C /tmp/$prog
+REV=$(git rev-parse --short HEAD)
 echo $(date +%Y%m%d)r$REV >/tmp/$prog/VERSION
 touch /tmp/$prog/conf/application-site.conf
 play dependencies /tmp/$prog --forceCopy --forProd
@@ -13,10 +13,8 @@ play precompile /tmp/$prog
 rm /tmp/$prog/conf/application-site.conf
 tar cfj /tmp/$prog-r$REV.tar.bz2 --exclude repo --exclude test -C /tmp $prog
 
-rm -fr /tmp/$prog
-
 if [ "ld-woodbridge.bc.ic.ac.uk" == "$HOSTNAME" ]; then
 	DEST=/data/www/80/cisbic.bioinformatics.ic.ac.uk/htdocs/files/$prog
 	scp /tmp/$prog-r$REV.tar.bz2 bss-srv4:$DEST
-	ssh bss-srv4 "sed -i 's/-r[0-9]\+/-r$REV/' $DEST/.htaccess"
+	ssh bss-srv4 "sed -i 's/-r[0-9a-f]\+/-r$REV/' $DEST/.htaccess"
 fi
