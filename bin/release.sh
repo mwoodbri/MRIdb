71f1472c3c7daf819c4abd5f6f913cc1bc0f7e26
Mark Woodbridge <m.woodbridge@imperial.ac.uk>
2013-08-06 10:40:45 +0100

    Git migration

--- bin/release.sh
+++ bin/release.sh
@@ -3,14 +3,14 @@
 cd "$(dirname "$0")"/..
 
 prog="mridb"
-REV=$(git rev-parse --short HEAD)
+VERSION=$(date +%Y%m%d)r$(git rev-parse --short HEAD)
 
 rm -fr /tmp/$prog
 mkdir /tmp/$prog
 git archive master | tar -x -C /tmp/$prog
-echo $(date +%Y%m%d)r$REV >/tmp/$prog/VERSION
+echo $VERSION >/tmp/$prog/VERSION
 touch /tmp/$prog/conf/application-site.conf
 play dependencies /tmp/$prog --forceCopy --forProd
 play precompile /tmp/$prog
 rm /tmp/$prog/conf/application-site.conf
-tar cfj /tmp/$prog-r$REV.tar.bz2 --exclude repo --exclude test -C /tmp $prog
+tar cfj /tmp/$prog-$VERSION.tar.bz2 --exclude repo --exclude test -C /tmp $prog
