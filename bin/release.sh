3b0b12b40eda7d7f7ad3752e7ffa5bd9e16223e0
Mark Woodbridge <1724545+mwoodbri@users.noreply.github.com>
2018-03-07 20:58:36 +0000

    Use HEAD to compile releases

--- bin/release.sh
+++ bin/release.sh
@@ -7,7 +7,7 @@ VERSION=$(date +%Y%m%d)r$(git rev-parse --short HEAD)
 
 rm -fr /tmp/$prog
 mkdir /tmp/$prog
-git archive master | tar -x -C /tmp/$prog
+git archive HEAD | tar -x -C /tmp/$prog
 echo $VERSION >/tmp/$prog/VERSION
 touch /tmp/$prog/conf/application-site.conf
 play dependencies /tmp/$prog --forceCopy --forProd
